9a91770bc2849aa4f246a653b966f68bb7f3a6c7
jwaddell-del <jwaddell@nd.edu>
2026-09-05 20:46:14 -0400

    Update Homework Chap 10 Math.R

--- Data/WebAssign_HW_Data/Homework Chap 10 Math.R
+++ Data/WebAssign_HW_Data/Homework Chap 10 Math.R
@@ -4,5 +4,5 @@
 
 ## Copy and paste the lines into an R Script window to read the data into RStudio.  
 
-math <- read.csv("https://www.crc.nd.edu/~jwaddell/ITAO20200/Data/WebAssign_HW_Data/MathTestScores.csv")
+math <- read.csv("https://github.com/Data-And-Stats-Course-Hub/ITAO-20200/raw/refs/heads/main/Data/WebAssign_HW_Data/MathTestScores.csv")
 math$location <- as.factor(math$location)
