0837cd44b578e77ac39e0b62c03939f4a2a04ad8
jwaddell-del <jwaddell@nd.edu>
2026-09-05 21:06:02 -0400

    Update Homework Chap 14 GPA Salary.R

--- Data/WebAssign_HW_Data/Homework Chap 14 GPA Salary.R
+++ Data/WebAssign_HW_Data/Homework Chap 14 GPA Salary.R
@@ -7,7 +7,7 @@
 
 
 ## gpasalary_r1
-gpasalary_r1<- read.csv("https://www.crc.nd.edu/~jwaddell/ITAO20200/Data/WebAssign_HW_Data/gpasalary_r1.csv")
+gpasalary_r1<- read.csv("https://github.com/Data-And-Stats-Course-Hub/ITAO-20200/raw/refs/heads/main/Data/WebAssign_HW_Data/gpasalary_r1.csv")
 Salary_model <- lm(Salary ~ GPA,          # Linear regression formula
                    data = gpasalary_r1)   # Name of the dataset
 library(olsrr)
@@ -16,7 +16,7 @@ ols_regress_mod(Salary_model)
 
 
 ## gpasalary_r2
-gpasalary_r2<- read.csv("https://www.crc.nd.edu/~jwaddell/ITAO20200/Data/WebAssign_HW_Data/gpasalary_r2.csv")
+gpasalary_r2<- read.csv("https://github.com/Data-And-Stats-Course-Hub/ITAO-20200/raw/refs/heads/main/Data/WebAssign_HW_Data/gpasalary_r2.csv")
 Salary_model <- lm(Salary ~ GPA,          # Linear regression formula
                    data = gpasalary_r2)   # Name of the dataset
 library(olsrr)
@@ -26,7 +26,7 @@ ols_regress_mod(Salary_model)
 
 
 ## gpasalary_r3
-gpasalary_r3<- read.csv("https://www.crc.nd.edu/~jwaddell/ITAO20200/Data/WebAssign_HW_Data/gpasalary_r3.csv")
+gpasalary_r3<- read.csv("https://github.com/Data-And-Stats-Course-Hub/ITAO-20200/raw/refs/heads/main/Data/WebAssign_HW_Data/gpasalary_r3.csv")
 Salary_model <- lm(Salary ~ GPA,          # Linear regression formula
                    data = gpasalary_r3)   # Name of the dataset
 library(olsrr)
@@ -36,7 +36,7 @@ ols_regress_mod(Salary_model)
 
 
 ## gpasalary_r4
-gpasalary_r4<- read.csv("https://www.crc.nd.edu/~jwaddell/ITAO20200/Data/WebAssign_HW_Data/gpasalary_r4.csv")
+gpasalary_r4<- read.csv("https://github.com/Data-And-Stats-Course-Hub/ITAO-20200/raw/refs/heads/main/Data/WebAssign_HW_Data/gpasalary_r4.csv")
 Salary_model <- lm(Salary ~ GPA,          # Linear regression formula
                    data = gpasalary_r4)   # Name of the dataset
 library(olsrr)
