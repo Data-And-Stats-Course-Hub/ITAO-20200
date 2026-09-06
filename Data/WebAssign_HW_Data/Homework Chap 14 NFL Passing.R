87c8bbcfa2c0b9d2a831b4094d360768f6ffcfbc
jwaddell-del <jwaddell@nd.edu>
2026-09-05 21:21:09 -0400

    Update Homework Chap 14 NFL Passing.R

--- Data/WebAssign_HW_Data/Homework Chap 14 NFL Passing.R
+++ Data/WebAssign_HW_Data/Homework Chap 14 NFL Passing.R
@@ -4,7 +4,7 @@
 
 # Paste all the code below into a script window and run it to perform the regression. 
 
-nflpassing_r<- read.csv("https://www.crc.nd.edu/~jwaddell/ITAO20200/Data/WebAssign_HW_Data/nflpassing_r.csv")
+nflpassing_r<- read.csv("https://github.com/Data-And-Stats-Course-Hub/ITAO-20200/raw/refs/heads/main/Data/WebAssign_HW_Data/nflpassing_r.csv")
 
 Passing_model <- lm(Win ~ Yds,            # Linear regression formula
                    data = nflpassing_r)   # Name of the dataset
