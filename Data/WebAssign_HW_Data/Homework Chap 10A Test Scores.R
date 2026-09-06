8fb35c7f72b8289d25ec8baa549b25874cb9f973
jwaddell-del <jwaddell@nd.edu>
2026-09-05 21:37:26 -0400

    Create Homework Chap 10A Test Scores.R

---
+++ Data/WebAssign_HW_Data/Homework Chap 10A Test Scores.R
@@ -0,0 +1,16 @@
+################################################################################
+#### Homework 10A, Test Scores                                              ####
+################################################################################
+
+# Choose the line of code that matches the dataset identified in your problem.
+# Paste it into a script window and run the line of code before writing the
+# rest of your code.
+
+
+testscores1_r <- read.csv("https://github.com/Data-And-Stats-Course-Hub/ITAO-20200/raw/refs/heads/main/Data/WebAssign_HW_Data/testscores1_r.csv")
+
+testscores2_r <- read.csv("https://github.com/Data-And-Stats-Course-Hub/ITAO-20200/raw/refs/heads/main/Data/WebAssign_HW_Data/testscores2_r.csv")
+
+testscores3_r <- read.csv("https://github.com/Data-And-Stats-Course-Hub/ITAO-20200/raw/refs/heads/main/Data/WebAssign_HW_Data/testscores3_r.csv")
+
+testscores4_r <- read.csv("https://github.com/Data-And-Stats-Course-Hub/ITAO-20200/raw/refs/heads/main/Data/WebAssign_HW_Data/testscores4_r.csv")
