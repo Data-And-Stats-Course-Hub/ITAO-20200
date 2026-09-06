1581d205cb8d8ce8e159d5e547b8c9cb21bee031
jwaddell-del <jwaddell@nd.edu>
2026-09-05 20:57:59 -0400

    Update Homework Chap 13 Auditor.R

--- Data/WebAssign_HW_Data/Homework Chap 13 Auditor.R
+++ Data/WebAssign_HW_Data/Homework Chap 13 Auditor.R
@@ -7,10 +7,10 @@
 # rest of your code.
 
 
-audjudg1_r<- read.csv("https://www.crc.nd.edu/~jwaddell/ITAO20200/Data/WebAssign_HW_Data/audjudg1_r.csv")
+audjudg1_r<- read.csv("https://github.com/Data-And-Stats-Course-Hub/ITAO-20200/raw/refs/heads/main/Data/WebAssign_HW_Data/audjudg1_r.csv")
 
-audjudg2_r<- read.csv("https://www.crc.nd.edu/~jwaddell/ITAO20200/Data/WebAssign_HW_Data/audjudg2_r.csv")
+audjudg2_r<- read.csv("https://github.com/Data-And-Stats-Course-Hub/ITAO-20200/raw/refs/heads/main/Data/WebAssign_HW_Data/audjudg2_r.csv")
 
-audjudg3_r<- read.csv("https://www.crc.nd.edu/~jwaddell/ITAO20200/Data/WebAssign_HW_Data/audjudg3_r.csv")
+audjudg3_r<- read.csv("https://github.com/Data-And-Stats-Course-Hub/ITAO-20200/raw/refs/heads/main/Data/WebAssign_HW_Data/audjudg3_r.csv")
 
-audjudg4_r<- read.csv("https://www.crc.nd.edu/~jwaddell/ITAO20200/Data/WebAssign_HW_Data/audjudg4_r.csv")
+audjudg4_r<- read.csv("https://github.com/Data-And-Stats-Course-Hub/ITAO-20200/raw/refs/heads/main/Data/WebAssign_HW_Data/audjudg4_r.csv")
