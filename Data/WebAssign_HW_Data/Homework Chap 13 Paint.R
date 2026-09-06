c618b3b983dfb356dfb06243af8fc156c3003882
jwaddell-del <jwaddell@nd.edu>
2026-09-05 21:01:10 -0400

    Update Homework Chap 13 Paint.R

--- Data/WebAssign_HW_Data/Homework Chap 13 Paint.R
+++ Data/WebAssign_HW_Data/Homework Chap 13 Paint.R
@@ -7,10 +7,10 @@
 # rest of your code.
 
 
-paint1_r<- read.csv("https://www.crc.nd.edu/~jwaddell/ITAO20200/Data/WebAssign_HW_Data/paint1_r.csv")
+paint1_r<- read.csv("https://github.com/Data-And-Stats-Course-Hub/ITAO-20200/raw/refs/heads/main/Data/WebAssign_HW_Data/paint1_r.csv")
 
-paint2_r<- read.csv("https://www.crc.nd.edu/~jwaddell/ITAO20200/Data/WebAssign_HW_Data/paint2_r.csv")
+paint2_r<- read.csv("https://github.com/Data-And-Stats-Course-Hub/ITAO-20200/raw/refs/heads/main/Data/WebAssign_HW_Data/paint2_r.csv")
 
-paint3_r<- read.csv("https://www.crc.nd.edu/~jwaddell/ITAO20200/Data/WebAssign_HW_Data/paint3_r.csv")
+paint3_r<- read.csv("https://github.com/Data-And-Stats-Course-Hub/ITAO-20200/raw/refs/heads/main/Data/WebAssign_HW_Data/paint3_r.csv")
 
-paint4_r<- read.csv("https://www.crc.nd.edu/~jwaddell/ITAO20200/Data/WebAssign_HW_Data/paint4_r.csv")
+paint4_r<- read.csv("https://github.com/Data-And-Stats-Course-Hub/ITAO-20200/raw/refs/heads/main/Data/WebAssign_HW_Data/paint4_r.csv")
