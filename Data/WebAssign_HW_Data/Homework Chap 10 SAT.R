12355bac3df344899aec33aa914738f5d7e23db5
jsakosa <120592096+jsakosa@users.noreply.github.com>
2026-09-07 16:01:20 -0400

    Update Homework Chap 10 SAT.R

--- Data/WebAssign_HW_Data/Homework Chap 10 SAT.R
+++ Data/WebAssign_HW_Data/Homework Chap 10 SAT.R
@@ -4,5 +4,5 @@
 
 ## Copy and paste the lines into an R Script window to read the data into RStudio.  
 
-sat <- read.csv("https://github.com/Data-And-Stats-Course-Hub/ITAO-20200/raw/refs/heads/main/Data/WebAssign_HW_Data/SAT.csv")
+sat <- read.csv("https://tinyurl.com/4s94c8hk)
 sat$group <- as.factor(sat$group)
