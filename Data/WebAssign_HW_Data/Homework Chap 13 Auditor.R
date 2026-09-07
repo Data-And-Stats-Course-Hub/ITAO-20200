c0b1e4a10ce94b94f50bfdfa23aef22953de2133
jsakosa <120592096+jsakosa@users.noreply.github.com>
2026-09-07 16:09:50 -0400

    Update Homework Chap 13 Auditor.R

--- Data/WebAssign_HW_Data/Homework Chap 13 Auditor.R
+++ Data/WebAssign_HW_Data/Homework Chap 13 Auditor.R
@@ -7,10 +7,10 @@
 # rest of your code.
 
 
-audjudg1_r<- read.csv("https://github.com/Data-And-Stats-Course-Hub/ITAO-20200/raw/refs/heads/main/Data/WebAssign_HW_Data/audjudg1_r.csv")
+audjudg1_r<- read.csv("https://tinyurl.com/mvvu34b6")
 
-audjudg2_r<- read.csv("https://github.com/Data-And-Stats-Course-Hub/ITAO-20200/raw/refs/heads/main/Data/WebAssign_HW_Data/audjudg2_r.csv")
+audjudg2_r<- read.csv("https://tinyurl.com/47ebk3v3")
 
-audjudg3_r<- read.csv("https://github.com/Data-And-Stats-Course-Hub/ITAO-20200/raw/refs/heads/main/Data/WebAssign_HW_Data/audjudg3_r.csv")
+audjudg3_r<- read.csv("https://tinyurl.com/4zkr694k")
 
-audjudg4_r<- read.csv("https://github.com/Data-And-Stats-Course-Hub/ITAO-20200/raw/refs/heads/main/Data/WebAssign_HW_Data/audjudg4_r.csv")
+audjudg4_r<- read.csv("https://tinyurl.com/22zkthm6")
