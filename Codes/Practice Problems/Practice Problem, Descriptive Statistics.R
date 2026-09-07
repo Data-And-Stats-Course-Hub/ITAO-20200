966101534ee07461d530330192ec587d342f3549
jwaddell-del <jwaddell@nd.edu>
2026-09-07 19:14:04 -0400

    Add files via upload

---
+++ Codes/Practice Problems/Practice Problem, Descriptive Statistics.R
@@ -0,0 +1,81 @@
+## Create the variables
+
+fruits <- c("orange","orange","orange","orange","orange","orange","orange",
+            "orange","orange","orange","orange","orange","orange","orange",
+            "orange","orange","orange","orange","orange","orange","orange",
+            "orange","orange","orange","orange","orange","orange","orange",
+            "orange","orange","apple","apple","apple","apple","apple","apple",
+            "apple","apple","apple","apple","apple","apple","apple","apple",
+            "apple","apple","apple","apple","apple","apple","apple","apple",
+            "apple","apple","apple","apple","apple","apple","apple","apple",
+            "apple","apple","apple","apple","apple","apple","apple","apple",
+            "apple","apple","apple","apple","apple","apple","apple","apple",
+            "apple","apple","apple","apple","apple","apple","apple","apple",
+            "apple","apple","apple","banana","banana","banana","banana","banana",
+            "banana","banana","banana","banana","banana","banana","banana",
+            "banana","banana","banana","banana","banana","banana","banana",
+            "banana","banana","banana","banana","banana","banana","banana",
+            "banana","banana","banana","banana","banana","banana","banana",
+            "banana","banana","banana","banana","banana","banana","banana",
+            "banana","banana","banana","banana","banana","banana","banana",
+            "banana","banana","banana","banana","banana","banana","banana",
+            "banana","banana","banana","banana","banana","banana","banana",
+            "banana","banana","banana","banana","banana","banana","banana",
+            "banana","banana","banana","banana","banana","banana","banana",
+            "banana","banana","banana","banana","banana","banana","banana",
+            "banana","banana","banana","banana","banana","banana","banana",
+            "banana","banana","banana","banana","banana","banana","banana",
+            "banana","banana","banana","banana","banana","banana","banana",
+            "banana","banana","banana","banana","banana","banana","banana",
+            "banana","pear","pear","pear","pear","pear","pear","pear","pear",
+            "pear","pear","pear","pear","pear","pear","pear","pear","pear",
+            "pear","pear","pear","pear","pear","pear","pear","pear","pear",
+            "pear","pear","pear","pear","pear","pear","pear","pear","pear",
+            "pear","pear","pear","pear","pear","pear","pear","pear","pear",
+            "grapes","grapes","grapes","grapes","grapes","grapes","grapes",
+            "grapes","grapes","grapes","grapes","grapes","grapes","grapes",
+            "grapes","grapes","grapes","grapes","grapes","grapes","grapes",
+            "grapes","grapes","grapes","grapes","grapes","grapes","grapes",
+            "grapes","grapes","grapes","grapes","grapes","grapes","grapes",
+            "grapes","grapes","grapes","grapes","grapes","grapes","grapes",
+            "grapes","grapes","grapes","grapes","grapes","grapes","grapes",
+            "grapes","grapes","grapes","grapes","grapes","grapes","grapes",
+            "grapes","grapes","grapes","grapes","grapes","grapes","grapes",
+            "grapes","grapes","grapes","grapes","grapes","grapes","grapes",
+            "grapes","grapes","grapes","grapes","grapes","grapes","grapes",
+            "grapes","grapes","grapes","grapes","grapes","grapes","grapes",
+            "grapes","grapes","grapes","grapes","grapes","grapes","grapes",
+            "grapes","grapes")
+grade <- c(3,3,3,4,5,2,3,2,2,1,2,3,2,2,1,1,3,4,5,5,4,4,5,4,3,2,2,1,1,2,5,5,5,5,
+           2,2,3,4,3,4,5,4,3,4,5,5,2,1,1,3,3,3,3,4,5,5,4,5,4,5,4,4,2,2,1,2,1,1,
+           3,2,2,3,4,4,4,4,5,3,5,4,5,4,5,2,4,1,4,1,2,3,2,3,1,1,3,2,3,4,3,2,5,3,
+           2,2,3,4,4,5,4,3,2,3,2,1,2,3,2,1,2,3,2,1,1,2,4,5,5,4,5,4,5,5,4,3,3,2,
+           3,4,4,5,4,5,4,4,3,4,2,1,4,2,4,3,2,4,5,5,3,2,3,4,5,3,3,2,2,1,1,5,5,4,
+           1,4,1,2,3,2,3,1,1,3,2,3,4,3,2,5,3,3,3,3,4,5,2,3,2,2,1,2,3,2,2,1,1,3,
+           4,5,5,2,1,1,3,3,3,3,4,5,5,4,5,4,5,4,4,2,2,1,2,1,1,3,4,5,5,4,4,5,4,3,
+           2,1,1,3,4,5,4,4,3,4,2,1,4,2,4,3,2,4,5,5,3,2,3,4,5,3,3,2,2,1,1,5,5,4,
+           2,3,2,2,1,2,3,5,4,4,3,4,2,1,4,2,4,3,2,4,5,5,3,2,3,4,5,3,3,2,2,1,1,5,
+           1,1,3,4,5,4,4,3,4,2,1,4,2,4,3,2,4,5,5,3,2,3,4,5,3,3,4,5,5)
+
+## Code for Question 1:
+
+
+
+
+## Code for Question 2:
+
+
+
+
+## Code for Question 3:
+
+
+
+
+## Code for Question 4:
+
+
+
+
+## Code for Question 5:
+
