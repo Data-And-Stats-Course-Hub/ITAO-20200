a557f4edb0473db139ce783fbfe3854cf9f1cb2c
jsakosa <120592096+jsakosa@users.noreply.github.com>
2026-09-07 16:15:15 -0400

    Update Homework Chap 13 Paint.R

--- Data/WebAssign_HW_Data/Homework Chap 13 Paint.R
+++ Data/WebAssign_HW_Data/Homework Chap 13 Paint.R
@@ -7,10 +7,10 @@
 # rest of your code.
 
 
-paint1_r<- read.csv("https://github.com/Data-And-Stats-Course-Hub/ITAO-20200/raw/refs/heads/main/Data/WebAssign_HW_Data/paint1_r.csv")
+paint1_r<- read.csv("https://tinyurl.com/ythzdbbv")
 
-paint2_r<- read.csv("https://github.com/Data-And-Stats-Course-Hub/ITAO-20200/raw/refs/heads/main/Data/WebAssign_HW_Data/paint2_r.csv")
+paint2_r<- read.csv("https://tinyurl.com/2hfmh2ht")
 
-paint3_r<- read.csv("https://github.com/Data-And-Stats-Course-Hub/ITAO-20200/raw/refs/heads/main/Data/WebAssign_HW_Data/paint3_r.csv")
+paint3_r<- read.csv("https://tinyurl.com/ykzyu7dd")
 
-paint4_r<- read.csv("https://github.com/Data-And-Stats-Course-Hub/ITAO-20200/raw/refs/heads/main/Data/WebAssign_HW_Data/paint4_r.csv")
+paint4_r<- read.csv("https://tinyurl.com/2rw3sxxk")
