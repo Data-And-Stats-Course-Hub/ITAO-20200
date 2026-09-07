b03be84235ad8c0a7e6ca8ba116dd99822ce5043
jsakosa <120592096+jsakosa@users.noreply.github.com>
2026-09-07 16:03:31 -0400

    Update Homework Chap 10A Test Scores.R

--- Data/WebAssign_HW_Data/Homework Chap 10A Test Scores.R
+++ Data/WebAssign_HW_Data/Homework Chap 10A Test Scores.R
@@ -7,10 +7,10 @@
 # rest of your code.
 
 
-testscores1_r <- read.csv("https://github.com/Data-And-Stats-Course-Hub/ITAO-20200/raw/refs/heads/main/Data/WebAssign_HW_Data/testscores1_r.csv")
+testscores1_r <- read.csv("https://tinyurl.com/48vbdsm5")
 
-testscores2_r <- read.csv("https://github.com/Data-And-Stats-Course-Hub/ITAO-20200/raw/refs/heads/main/Data/WebAssign_HW_Data/testscores2_r.csv")
+testscores2_r <- read.csv("https://tinyurl.com/mp7htpaf")
 
-testscores3_r <- read.csv("https://github.com/Data-And-Stats-Course-Hub/ITAO-20200/raw/refs/heads/main/Data/WebAssign_HW_Data/testscores3_r.csv")
+testscores3_r <- read.csv("https://tinyurl.com/3frw9ukm")
 
-testscores4_r <- read.csv("https://github.com/Data-And-Stats-Course-Hub/ITAO-20200/raw/refs/heads/main/Data/WebAssign_HW_Data/testscores4_r.csv")
+testscores4_r <- read.csv("https://tinyurl.com/3u69n685")
