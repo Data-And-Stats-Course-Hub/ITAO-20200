8031e37a7901e6a363db5b05859f5269f0020cd4
jwaddell-del <jwaddell@nd.edu>
2026-09-26 15:42:30 -0400

    Update Code for Single Sample t Test.R

--- R-Guides-and-Codes/Code for Single Sample t Test.R
+++ R-Guides-and-Codes/Code for Single Sample t Test.R
@@ -27,6 +27,6 @@ t.test(Grades$Pretest,            # the variable we are testing
        conf.level = 0.95)         # the confidence level = 1 - alpha
 
 # The "alternative" argument controls which test you're running:
-#   alternative = "less"      -> lower-tailed test   (Ha: mu < 70)
+#   alternative = "less"      -> lower-tailed test    (Ha: mu < 70)
 #   alternative = "greater"   -> upper-tailed test    (Ha: mu > 70)
 #   alternative = "two.sided" -> two-tailed test      (Ha: mu != 70)
