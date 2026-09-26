98aa8e8f9408bd05d23a352c30a0e0503e724e84
jsakosa <120592096+jsakosa@users.noreply.github.com>
2026-09-26 14:46:08 -0400

    Update Codes for Single Sample AND Dependent t Tests.R

--- R-Guides-and-Codes/Codes for Single Sample AND Dependent t Tests.R
+++ R-Guides-and-Codes/Codes for Single Sample AND Dependent t Tests.R
@@ -27,6 +27,11 @@ t.test(Grades$Pretest,            # the variable we are testing
        mu = 70,                   # the null value
        conf.level = 0.95)         # the confidence level = 1 - alpha
 
+# The "alternative" argument controls which test you're running:
+#   alternative = "less"      -> lower-tailed test   (Ha: mu < 70)
+#   alternative = "greater"   -> upper-tailed test    (Ha: mu > 70)
+#   alternative = "two.sided" -> two-tailed test      (Ha: mu != 70)
+
 #################################################################
 # PART 2: DEPENDENT (PAIRED SAMPLES) t TEST
 #################################################################
